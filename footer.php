790fa4afcb3a5ea624e1fa50e581e62c344ec329
Eugeny <killerdjek@gmail.com>
2020-01-30 12:06:55 +0300

    First commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+		<footer>
+			<h1>Test</h1>
+		</footer>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<title>ЭлитСтрой</title>
+</head>
+<body>
+	<header>
+		<h1><span class="mainstrok">Govno</span></h1>
+	</header>
+
+	
