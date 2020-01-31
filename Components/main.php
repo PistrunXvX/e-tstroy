337862e95d498ef20bb49c573dae7d721cd3fb7e
Eugeny <killerdjek@gmail.com>
2020-01-31 23:27:09 +0300

    Done style main.php

--- Components/main.php
+++ Components/main.php
@@ -1,5 +1,5 @@
-<main class="container-fluid">
-	<div class="row text-main">
+<main class="container-fluid main-color">
+	<div class="row text-main text-top">
 		<div class="col">
 			<p>
 				Представляем Вам нашу Группу Компаний. Мы осуществляем свою
@@ -9,7 +9,7 @@
 			</p>
 		</div>
 	</div>
-	<div class="row text-main">
+	<div class="row text-main text-top">
 		<div class="col">
 			<p>
 				Мы осуществляем весь спектр работ, направленных на выполнение
@@ -32,33 +32,36 @@
 	</div>
 	<section class="company-org-container">
 		<div class="row company-org">
-			<div class="col company-org">
+			<div class="col company-org main-text-company">
 				<h2>В Группу Компаний входят следующие организации:</h2>
 			</div>
 		</div>
 		<div class="row company-org">
-			<div class="col-6">
-				<section class="about-1">
+			<div class="col-4">
+				<section class="about-1 about-container">
 					<h3>Отделочные работы</h3>
 					<p>«КапРегионСтрой»</p>
 				</section>
 			</div>
-			<div class="col-6">
-				<section class="about-2">
+			<div class="col-4">
+				<section class="about-2 about-container">
 					<h3>Проектирование и монтаж внутренних инженерных</h3>
 					<p>«Технострой»</p>
 				</section>
 			</div>
+			<div class="col-4 back-img-company">
+				
+			</div>
 		</div>
 		<div class="row company-org">
-			<div class="col-6">
-				<section class="about-3">
+			<div class="col-4">
+				<section class="about-3 about-container">
 					<h3>Производство и монтаж окон</h3>
 					<p>«Окна-эксперт»</p>
 				</section>
 			</div>
-			<div class="col-6">
-				<section class="about-4">
+			<div class="col-4">
+				<section class="about-4 about-container">
 					<h3>Поставка строительных и отделочных материалов</h3>
 					<p>«ТД Покров»</p>
 				</section>
@@ -66,7 +69,12 @@
 		</div>
 	</section>
 	<section class="report">
-		<!-- Добавить слайдер отзывов -->
+		<div class="row">
+			<div class="col">
+				<h2>Отзывы</h2>
+						<!-- Добавить слайдер отзывов -->	
+			</div>
+		</div>
 	</section>
 	<section class="project">
 		<div class="row project-text">
@@ -90,7 +98,7 @@
 					<a href="#">
 						<img src="img/Poekt_2.png" class="img-fluid" alt="строительство завода по производству газоизмерительных станций (ГИС)">
 						<p>
-							строительство завода по производству газоизмерительных станций (ГИС)
+							строительство завода ГИС
 						</p>
 					</a>
 				</section>
@@ -111,9 +119,11 @@
 				<p>
 					И еще более 60 выполненых работ
 				</p>
-				<p>
-					Убедитесь в этом в нашем <a href="#" class="portfolio-text-href">портфолио</a> 
-				</p>
+				<section class="right-text">
+					<p>
+						Убедитесь в этом в нашем <a href="#" class="portfolio-text-href">портфолио</a> 
+					</p>
+				</section>
 			</div>
 		</div>
 	</section>
@@ -121,14 +131,18 @@
 		<div class="row license-text">
 			<div class="col">
 				<h2>Лицензия</h2>
-				<img src="img/unnamed.png" class="img-fluid" alt="Альфа-партнер центр рецензирования">
+				<img src="img/unnamed.png" class="img-fluid mx-auto d-block" alt="Альфа-партнер центр рецензирования">
+			</div>
+		</div>
+		<div class="row license-text-href">
+			<div class="col">
 				<a href="#">Выписка из СРО</a>
 			</div>
 		</div>
 	</section>
 	<section class="contact">
 		<div class="row">
-			<div class="col-3">
+			<div class="col-3 contacts-company">
 				<section class="contacts-company">
 					<section class="phone">
 						<p>
@@ -144,17 +158,17 @@
 					</section>
 				</section>
 			</div>
-			<div class="col-6">
-				<section class="contact-me">
+			<div class="col-6 contact-me">
+				<section>
 					<h2>Оставтье заявку</h2>
 					<form action="">
-						<input type="email">
-						<input type="tel">
-						<button><img src="img/paper-plane.svg" alt="Отправить заявку"></button>
+						<input type="email" placeholder="Ваш email">
+						<input type="tel" placeholder="Ваш телефон">
+						<button></button>
 					</form>
 				</section>
 			</div>
-			<div class="col-3">
+			<div class="col-3 addres-company">
 				<section class="addres-company">
 					<p>
 						Мы находимся
