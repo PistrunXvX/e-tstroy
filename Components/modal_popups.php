261c8bd12d6117f45c0a36844c0ed7720953ea1e
Eugeny <killerdjek@gmail.com>
2020-02-03 20:39:56 +0300

    POP up

---
+++ Components/modal_popups.php
@@ -0,0 +1,20 @@
+
+<div class="modal fade" id="project_1" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Магазин шаговой доступности</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img src="../img/project_img/project_1_front-min.jpg">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
--- portfolio.php
+++ portfolio.php
@@ -1,5 +1,7 @@
 <?php
+require('Components/modal_popups.php');
 require("Components/header.php");
 require("Components/main_portfolio.php");
+require('Components/portfolio_project.php');
 require("Components/footer.php");
 ?>
