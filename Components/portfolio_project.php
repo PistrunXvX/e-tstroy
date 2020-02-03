6c2287d3319d07128afb909a5dcf678ae349f6b8
pentagonich <serwillow.wolfe@gmail.com>
2020-02-03 16:29:30 +0300

    Added sliders form

--- Components/portfolio_project.php
+++ Components/portfolio_project.php
@@ -1,7 +1,8 @@
 <link rel="stylesheet" href="../css/style_portfolio.css">
 <div class="container-fluid project-back">
 	<div class="container portfolio-template">
-		<section class="fixed-button"><button id="btn"></button></section>
+		<section class="fixed-button"><button id="btn_one" onclick=""></button></section>
+		<section class="second-button"><button id="btn_two" onclick="goback()"></button></section>
 			
 		
 		<div class="slider">
@@ -108,6 +109,34 @@
 					</div>
 				</div>
 			</div>
+
+			<div class="item_two" id="itm_two">
+
+				<div class="row no-gutters">
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+
+					<div class="item_three" id="itm_three">
+
+				<div class="row no-gutters">
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					
+
 
 			
 
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,11 +1,4 @@
-document.getElementById('btn').onclick = function slideing() {
-      			//document.getElementById('itm').style.display = 'none';
-      			let slide = document.getElementById('itm');
-      			 if (slide.style.display === 'none') {
-        			slide.style.display = 'block';
-    			} else {
-        			slide.style.display = 'none';
-    }
-}
-      		
-      		
+
+
+		
+	
