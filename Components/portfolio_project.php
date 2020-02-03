f0b414d53bc1d0e2c34f331f5c9769c5e17bb535
pentagonich <serwillow.wolfe@gmail.com>
2020-02-03 08:41:47 +0300

    Added slide return

--- Components/portfolio_project.php
+++ Components/portfolio_project.php
@@ -93,6 +93,18 @@
 							Описание обьекта
 						</p>
 					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
 					</div>
 				</div>
 			</div>
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,3 +1,11 @@
 document.getElementById('btn').onclick = function slideing() {
-      			document.getElementById('itm').style.display = 'none';
-      		}
+      			//document.getElementById('itm').style.display = 'none';
+      			let slide = document.getElementById('itm');
+      			 if (slide.style.display === 'none') {
+        			slide.style.display = 'block';
+    			} else {
+        			slide.style.display = 'none';
+    }
+}
+      		
+      		
