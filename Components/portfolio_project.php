449f452e82f77174b49097b2bd4c786f71b5ac63
Eugeny <killerdjek@gmail.com>
2020-02-03 06:02:49 +0300

    Add button main_portfolio

--- Components/portfolio_project.php
+++ Components/portfolio_project.php
@@ -1,8 +1,57 @@
 <link rel="stylesheet" href="../css/style_portfolio.css">
 <div class="container-fluid project-back">
+	<button class=""></button>
 	<div class="container portfolio-template">
 		<div class="row no-gutters">
 			<div class="col-3" id="">
+				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+				<p>
+					Описание обьекта
+				</p>
+			</div>
+			<div class="col-3" id="">
+				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+				<p>
+					Описание обьекта
+				</p>
+			</div>
+			<div class="col-3" id="">
+				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+				<p>
+					Описание обьекта
+				</p>
+			</div>
+			<div class="col-3" id="">
+				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+				<p>
+					Описание обьекта
+				</p>
+			</div>
+			<div class="col-3" id="">
+				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+				<p>
+					Описание обьекта
+				</p>
+			</div>
+						<div class="col-3" id="">
+				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+				<p>
+					Описание обьекта
+				</p>
+			</div>
+						<div class="col-3" id="">
+				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+				<p>
+					Описание обьекта
+				</p>
+			</div>
+						<div class="col-3" id="">
+				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+				<p>
+					Описание обьекта
+				</p>
+			</div>
+						<div class="col-3" id="">
 				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
 				<p>
 					Описание обьекта
