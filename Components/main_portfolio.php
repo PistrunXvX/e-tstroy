d0c99a1d96e4ab8585b3ce73e2dba0f5e25fd147
Eugeny <killerdjek@gmail.com>
2020-02-03 03:35:51 +0300

    style input in portfolio_main

--- Components/main_portfolio.php
+++ Components/main_portfolio.php
@@ -1,16 +1,20 @@
 <section class="project-back">
 	<div class="container">
 		<div class="row">
-			<div class="col">
+			<div class="col-6 project-portfolio-back">
 				<section class="project-filter">
 					<p>
 						Выберите категорию
-					</p>
-					<select name="" id="">
+					</p>	
+				</section>
+			</div>
+			<div class="col-6 project-portfolio-back input-portfolio">
+				<section>
+						<select name="" id="">
 						<option value="">Жилые дома</option>
 						<option value="">Заводские сооружения</option>
-					</select>
-				</section>	
+					</select>	
+					</section>
 			</div>
 		</div>
 	</div>
