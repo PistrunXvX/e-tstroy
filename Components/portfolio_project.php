6cf8e580fabedca8215754efe27724cefc08b631
pentagonich <serwillow.wolfe@gmail.com>
2020-02-03 08:28:06 +0300

    Added slide function for one button

--- Components/portfolio_project.php
+++ Components/portfolio_project.php
@@ -1,149 +1,106 @@
 <link rel="stylesheet" href="../css/style_portfolio.css">
 <div class="container-fluid project-back">
 	<div class="container portfolio-template">
-		<section class="fixed-button"><button class=""></button></section>
+		<section class="fixed-button"><button id="btn"></button></section>
+			
 		
-		<div class="row no-gutters">
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
+		<div class="slider">
+
+			<div class="item" id="itm">
+
+				<div class="row no-gutters">
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					<div class="col-3" id="">
+						<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
+						<p>
+							Описание обьекта
+						</p>
+					</div>
+					</div>
+				</div>
 			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-<<<<<<< HEAD
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-=======
-						<div class="col-3" id="">
->>>>>>> 449f452e82f77174b49097b2bd4c786f71b5ac63
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-<<<<<<< HEAD
-			<div class="col-3" id="">
-=======
-						<div class="col-3" id="">
->>>>>>> 449f452e82f77174b49097b2bd4c786f71b5ac63
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-<<<<<<< HEAD
-			<div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div><div class="col-3" id="">
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-			<div class="col-3" id="">
-=======
-						<div class="col-3" id="">
->>>>>>> 449f452e82f77174b49097b2bd4c786f71b5ac63
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-<<<<<<< HEAD
-			<div class="col-3" id="">
-=======
-						<div class="col-3" id="">
->>>>>>> 449f452e82f77174b49097b2bd4c786f71b5ac63
-				<img src="../img/project_img/IMG_0159.jpg" alt="" class="img-template">
-				<p>
-					Описание обьекта
-				</p>
-			</div>
-<<<<<<< HEAD
+
 			
 
-=======
->>>>>>> 449f452e82f77174b49097b2bd4c786f71b5ac63
+			
+
 		</div>
 	</div>
 </div>
---
+++ scripts/slider.js
@@ -0,0 +1,3 @@
+document.getElementById('btn').onclick = function slideing() {
+      			document.getElementById('itm').style.display = 'none';
+      		}
