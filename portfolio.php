5eee8341b1d3fac8af7c7e494365f76deb90536c
Eugeny <killerdjek@gmail.com>
2020-02-05 13:42:22 +0300

    Update bg portfolio

--- portfolio.php
+++ portfolio.php
@@ -1,6 +1,5 @@
 <?php
 require "Components/header.php";
-require "Components/main_portfolio.php";
 require 'Components/portfolio_project.php';
 require 'Components/modal_popups.php';
 require "Components/footer.php";
